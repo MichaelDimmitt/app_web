0419f7e9eb5b384ec77ce453aa3150f8bb30e1ce
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-12-03 21:51:53 -0500

    insert meta tag and update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,70 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
+import css from "../css/app.css";
 import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
 import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import {LiveSocket, debug, View} from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+let Hooks = {}
+
+Hooks.PhoneNumber = {
+  mounted(){
+    let pattern = /^(\d{3})(\d{3})(\d{4})$/
+    this.el.addEventListener("input", e => {
+      let match = this.el.value.replace(/\D/g, "").match(pattern)
+      if(match) {
+        this.el.value = `${match[1]}-${match[2]}-${match[3]}`
+      }
+    })
+  }
+}
+
+let scrollAt = () => {
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+  let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+  let clientHeight = document.documentElement.clientHeight
+
+  return scrollTop / (scrollHeight - clientHeight) * 100
+}
+
+Hooks.InfiniteScroll = {
+  page() { return this.el.dataset.page },
+  mounted(){
+    this.pending = this.page()
+    window.addEventListener("scroll", e => {
+      if(this.pending == this.page() && scrollAt() > 90){
+        this.pending = this.page() + 1
+        this.pushEvent("load-more", {})
+      }
+    })
+  },
+  updated(){ this.pending = this.page() }
+}
+
+let serializeForm = (form) => {
+  let formData = new FormData(form)
+  let params = new URLSearchParams()
+  for(let [key, val] of formData.entries()){ params.append(key, val) }
+
+  return params.toString()
+}
+
+let Params = {
+  data: {},
+  set(namespace, key, val){
+    if(!this.data[namespace]){ this.data[namespace] = {}}
+    this.data[namespace][key] = val
+  },
+  get(namespace){ return this.data[namespace] || {} }
+}
+
+Hooks.SavedForm = {
+  mounted(){
+    this.el.addEventListener("input", e => {
+      Params.set(this.viewName, "stashed_form", serializeForm(this.el))
+    })
+  }
+}
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
+
 liveSocket.connect()
